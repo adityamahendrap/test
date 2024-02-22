8608257afdb96ea0728c2ee30e9deedc97da7622
root <root@vps.winkwinkwinkz.cloud>
2024-02-22 19:14:39 +0700

    update

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ const port = 3000;
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Hello from server!");
+  res.send("Hello from server!\n");
 });
+
+app.get("/hello", (req, res) => {
+  res.send(`hello ${req.query.name}\n`);
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
